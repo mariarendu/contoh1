5ac3c609ab411361cac2ee1a546a98406b2531b4
mariarendu <renduuana.@gmail.com>
2023-11-05 01:57:15 +0700

    menambahkan welcoome.php,contoh1.php,index.html,latihan1.php

---
+++ contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ana Rendu
+             saya tinggal di daerah Kramat
+             kegiatan yang saya sukai adalah
+             bernyanyi";
+            
+    }
+}
